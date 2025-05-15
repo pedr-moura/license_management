a010cc2d53b655a7909a4991b9bb83861aa8d2f0
Pedro Moura <105287149+pedr-moura@users.noreply.github.com>
2025-05-15 19:47:31 -0300

    Update use.ps1

--- use.ps1
+++ use.ps1
@@ -182,6 +182,7 @@ $simpleHtmlContent = @"
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" />
     <link rel="stylesheet" href="https://lic-management.vercel.app/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 </head>
 <body>
 
